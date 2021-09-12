83f968afbb4d9fccba88a2397f23e9b52bed98ac
Kubakubikula <jakub.zient@gmail.com>
2021-09-12 21:34:59 +0100

    chess python react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'chessboard-element';
+
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
+      <div>
+        <h1>&lt;chess-board&gt; and React</h1>
+        <p>Here is &lt;chess-board&gt; inside a React app!</p>
+        <div style={{width : '400px', margin: '0 auto'}}>
+          <chess-board
+            position="start"
+            draggable-pieces
+            >
+          </chess-board>
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        ... no changes in this part ...
+
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
