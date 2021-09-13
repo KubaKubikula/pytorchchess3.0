10195d13f4de2097fc3688d54364e402dd9c2bed
Kubakubikula <jakub.zient@gmail.com>
2021-09-13 17:38:14 +0100

    it plays basic movements

--- api/app.py
+++ api/app.py
@@ -4,20 +4,27 @@ import chess
 app = Flask(__name__)
 board = chess.Board()
 
+# nějaké cookie které bude držet aktuální stav ?
+
+def is_valid_movement(source, target):
+    move = chess.Move.from_uci(source + target)
+    return move in board.legal_moves
+
 @app.route('/actualstate')
 def get_actual_state():
     return {'actualstate': board.fen()}
 
 @app.route('/restart')
 def restart():
-    board = chess.Board()
+    board.reset()
     return {'actualstate': board.fen()}
 
-@app.route('/move/<string:position>')
-def move(position):
+@app.route('/move/<string:source>/<string:target>')
+def move(source, target):
     try:
-        board.push_san(position)
-    except:
-        return {'validMove': 'False'}
-    return {'validMove': 'True'}
-
+        if is_valid_movement(source, target) != True:
+            return {'validMove': 'False', 'error' : 'wrong movement'}
+        board.push_san(target)
+    except ValueError as e:
+        return {'validMove': 'False', 'error' : str(e)}
+    return {'validMove': 'True', 'error' : 'False'}
--- src/App.js
+++ src/App.js
@@ -6,26 +6,55 @@ import 'chessboard-element';
 
 function App () {
   const [currentTime, setCurrentTime] = useState(0);
-  let actualState = "start";
+  let actualState = 'start';
+  let logareamessages = '';
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
+    const board = document.querySelector('chess-board');
+    const startButton = document.getElementById('startBtn');
+    const logarea = document.getElementById('logarea');
 
     fetch('/actualstate').then(res => res.json()).then(data => {
       actualState = data.actualstate;
     });
 
-    document.getElementById('startBtn').addEventListener('click',
-    () => document.getElementById('board3').start());
+    startButton.addEventListener('click',
+    () => {
+      board.start();
+      fetch('/restart');
+    });
+
+    board.addEventListener('drop', (e) => {
+      const {source, target, piece, newPosition, oldPosition, orientation} = e.detail;
+      console.log('Source: ' + source)
+      console.log('Target: ' + target)
+      console.log('Piece: ' + piece)
+      console.log('New position: ' + newPosition)
+      console.log('Old position: ' + oldPosition)
+      console.log('Orientation: ' + orientation)
+      console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+      fetch('/move/' + source + '/' + target).then(res => res.json()).then(data => {
+        logarea.innerHTML = logarea.innerHTML + data.error + '\n\n';
+
+        if(data.validMove == 'False') {
+          board.move(target + '-' + source);
+        }
+      });
+    });
   }, []);
 
   return (
     <div className="App">
       <div>
         <h1>&lt;chess-board&gt; and React</h1>
-        <p>Here is &lt;chess-board&gt; inside a React app!</p>
+        <div style={{float :'left'}}>
+          <strong>White: Human</strong><br />
+          Black: Human<br />
+          <textarea id="logarea" rows="30" cols="40" value={logareamessages} >
+          
+          </textarea>
+        </div>
         <div style={{width : '400px', margin: '0 auto'}}>
           <chess-board id="board3"
             position={actualState}
@@ -33,13 +62,12 @@ function App () {
             >
           </chess-board>
         </div>
+        <div style={{clear:'both'}}></div>
       </div>
       <button id="startBtn">Start Position</button>
       <header className="App-header">
 
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}. {actualState}</p>
+        <p></p>
       </header>
     </div>
   );
